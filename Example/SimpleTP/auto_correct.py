b5ddcfb55de0322301625d14e323d0e1dd03ba4b
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-12-04 23:04:21 -0500

    Update auto_correct.py

--- Example/SimpleTP/auto_correct.py
+++ Example/SimpleTP/auto_correct.py
@@ -8,10 +8,16 @@ except ImportError:
 
 
 def auto_correct():
-    code_source = tac.SourceCode(os.path.join(os.path.dirname(__file__), "..", "src"))
-    tests_source = tac.SourceTests(os.path.join(os.path.dirname(__file__), "..", "tests"))
-    auto_corrector = tac.AutoCorrector(code_source, tests_source)
-    auto_corrector.run()
+    code_source = tac.SourceCode(os.path.join(os.path.dirname(__file__), "src"))
+    print(code_source)
+    code_source.setup_at(os.path.join(os.path.dirname(__file__), "temp"), overwrite=True)
+    print(code_source)
+    tests_source = tac.SourceTests(os.path.join(os.path.dirname(__file__), "tests"))
+    print(tests_source)
+    tests_source.setup_at(os.path.join(os.path.dirname(__file__), "temp"), overwrite=True)
+    print(tests_source)
+    # auto_corrector = tac.AutoCorrector(code_source, tests_source)
+    # auto_corrector.run()
 
 
 if __name__ == "__main__":
