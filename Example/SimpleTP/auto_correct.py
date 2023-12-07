23347704ed96d580a9cc94d919cb1dd709091c25
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-12-06 22:29:10 -0500

    Update auto_correct.py

--- Example/SimpleTP/auto_correct.py
+++ Example/SimpleTP/auto_correct.py
@@ -19,6 +19,23 @@ def auto_correct():
     print(auto_corrector.report)
 
 
+def auto_correct_from_git():
+    path_to_example = os.path.join(".", "Example")
+    repo_url = "https://github.com/JeremieGince/TPAutoCorrect.git"
+    code_source = tac.SourceCode(os.path.join(path_to_example, "src"), url=repo_url)
+    print(code_source)
+    code_source.setup_at("./temp")
+    print(code_source)
+    # tests_source = tac.SourceTests(os.path.join(path_to_example, "tests"), url=repo_url)
+    # print(tests_source)
+    # master_tests_source = tac.SourceTests(os.path.join(path_to_example, "master_tests"), url=repo_url)
+    # print(master_tests_source)
+    # auto_corrector = tac.Tester(code_source, tests_source, master_tests_src=master_tests_source)
+    # auto_corrector.run(overwrite=True, debug=True)
+    # print(auto_corrector.report)
+
+
 if __name__ == "__main__":
-    auto_correct()
+    # auto_correct()
+    auto_correct_from_git()
 
