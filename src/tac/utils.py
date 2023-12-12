dd82d437e0411387a8c09004b326a8546dade748
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-12-11 19:46:26 -0500

    Update utils.py

--- src/tac/utils.py
+++ src/tac/utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 from typing import Optional, Union, List
 
@@ -106,5 +107,59 @@ def rm_pytest_cache(root: Optional[str] = None):
     return rm_direnames_from_root(".pytest_cache", root=root)
 
 
+def reindent_json_file(filepath: str, indent: int = 4, dont_exist_ok: bool = True):
+    import json
+
+    if not os.path.exists(filepath):
+        if dont_exist_ok:
+            return None
+        raise FileNotFoundError(f"File {filepath} does not exist")
+    if not os.path.isfile(filepath):
+        raise ValueError(f"File {filepath} must be a file.")
+
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=indent)
+    return filepath
+
+
+def is_file_in_dir(filename: str, dirpath: str) -> bool:
+    for root, dirs, files in os.walk(dirpath):
+        for file in files:
+            if file == filename:
+                return True
+    return False
+
+
+def is_subpath_in_path(subpath: str, path: str) -> bool:
+    subpath = os.path.abspath(subpath)
+    path = os.path.abspath(path)
+    return subpath in path
+
+
+def import_obj_from_file(obj_name: str, filepath: str):
+    from importlib import util as importlib_util
+
+    spec = importlib_util.spec_from_file_location("module.name", filepath)
+    foo = importlib_util.module_from_spec(spec)
+    len_root = len(filepath.split(os.path.sep))
+    import_root = os.path.dirname(filepath)
+    for _ in range(len_root):
+        try:
+            spec.loader.exec_module(foo)
+        except (ImportError, ModuleNotFoundError):
+            sys.path.append(os.path.dirname(import_root))
+        else:
+            break
+        import_root = os.path.dirname(import_root)
+    obj = getattr(foo, obj_name)
+
+    # filepath = os.path.normpath(filepath)
+    # relative_path = os.path.relpath(filepath, os.getcwd())
+    # module_name_ext = relative_path.replace(os.path.sep, ".")
+    # module_name = module_name_ext.rsplit(".", 1)[0]
+    # obj = getattr(importlib.import_module(module_name), obj_name)
+    return obj
 
 
